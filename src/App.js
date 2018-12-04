05e7aac4eb955f18533f846fa9e1bf327b2651c3
Szymon Oleszek <szymqwerty@gmail.com>
2018-12-04 21:29:18 +0100

    created store and initial rootreducer to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1 className="display-4">ITAD</h1>
-      </div>
+      <Provider store={store}>
+        <div>
+          <h1 className="display-4">ITAD</h1>
+        </div>
+      </Provider>
     );
   }
 }
